b5870c63bbb6c06cabf11059b2132d3ec13d360b
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2021-12-14 08:04:20 -0300

    crud completo en front

--- initNodejs/public/js/prueba.js
+++ initNodejs/public/js/prueba.js
@@ -6,6 +6,7 @@ const userPassword = document.getElementById('user-password');
 const userEmail = document.getElementById('user-email');
 const userAddress = document.getElementById('user-address');
 const userPhone = document.getElementById('user-phone');
+const userId = document.getElementById('user-id');
 const texto = document.getElementById('texto');
 
 //Send POST to API to add store
@@ -37,12 +38,13 @@ async function addUser(e) {
         console.log(sendBody)
         if(res.status === 400){
           // alert('holi');
-          throw Error('Store alredy exist.');
+          throw Error('User alredy exist.');
         }
     
         // alert('Store added!');
         // window.location.href = 'index.html';
-    
+        location.reload();
+
       } catch (err) {
         alert(err);
         return;
@@ -52,7 +54,7 @@ async function addUser(e) {
 async function getUser(e) {
   e.preventDefault();
 
-  const res = await fetch('/api/users');
+  const res = await fetch('/api/users');//El metodo get va por defecto y los header son opcionales
   const users = await res.json();
 
   const strings = users.map(user => JSON.stringify(user,null,4));
@@ -60,5 +62,68 @@ async function getUser(e) {
   texto.innerText = strings;
 }
 
+
+
+async function putUser(e) {
+  e.preventDefault();
+
+  if(userFirstName.value ==='' || userLastName.value ==='' || userName.value ==='' || userPassword.value ==='' || userEmail.value ==='' || userAddress.value ==='' || userPhone.value ==='' ) {
+      alert('Porfa llena los campos');
+  }
+
+  const sendBody = {
+      firstName: userFirstName.value,
+      lastName: userLastName.value,
+      userName: userName.value,
+      password: userPassword.value,
+      email: userEmail.value,
+      address: userAddress.value,
+      phone: userPhone.value
+  }
+  
+  try {
+      const res = await fetch(`/api/users/${userId.value}`, {
+        method: 'PUT',
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(sendBody)
+      });
+      console.log(sendBody)
+      if(res.status === 400){
+        // alert('holi');
+        throw Error('User alredy exist.');
+      }
+  
+      // alert('Store added!');
+      // window.location.href = 'index.html';
+  
+    } catch (err) {
+      alert(err);
+      return;
+    }
+}
+async function deleteUser(e) {
+  e.preventDefault();
+  let isDelete = confirm(`Estás seguro que deseas eliminar el id ${userId.value}?`);
+
+  if(isDelete){
+    try {
+        const res = await fetch(`/api/users/${userId.value}`, {
+          method: 'DELETE',
+          headers: { 
+            'Content-Type': 'application/json'
+          },
+        });
+        } catch (err) {
+          alert(err);
+          return;
+        }
+    location.reload();
+  }
+}
+
 userForm.querySelector('#post').addEventListener('click', addUser);
 userForm.querySelector('#get').addEventListener('click', getUser);
+userForm.querySelector('#put').addEventListener('click', putUser);
+userForm.querySelector('#delete').addEventListener('click', deleteUser);
